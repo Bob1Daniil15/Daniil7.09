ee9906a852f073b65cde94743ffcc99291282285
Daniil <144154258+Bob1Daniil15@users.noreply.github.com>
2023-09-17 11:39:25 +0300

    Update Daniil7.09.cpp

--- Daniil7.09/Daniil7.09.cpp
+++ Daniil7.09/Daniil7.09.cpp
@@ -1,8 +1,56 @@
-﻿
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    string getResponse(const string& message) {
+    if (message == "Привет") {
+        return "Привет! Как ты?";
+    }
+    else if (message == "Пока") {
+        return "Пока! Увидимся в следующий раз!";
+    }
+    else {
+            vector<string> randomResponses = {
+            "А ты молодец!",
+            "Я не уверен, что понимаю. Можешь объяснить подробнее?",
+            "Как интересно! Расскажи больше.",
+            "Что ты имеешь в виду?",
+            "Мне кажется, это интересная тема.",
+            "Понятно. А что ты думаешь об этом?"
+        };
+
+        int numResponses = randomResponses.size();
+        int randomIndex = rand() % numResponses;
+
+        return randomResponses[randomIndex];
+    }
 }
 
+int main() {
+        setlocale(LC_ALL, "Russian");
+        srand(time(0));
+
+        string message;
+        cout << "Привет! Чат-бот готов к общению." << endl;
+
+    while (true) {
+            cout << "> ";
+            getline(cin, message);
+
+        if (message.empty()) {
+            continue;
+        }
+
+            string response = getResponse(message);
+            cout << "Бот: " << response << std::endl;
+
+        if (response == "Пока! Увидимся в следующий раз!") {
+            break;
+        }
+    }
+
+    return 0;
+}
